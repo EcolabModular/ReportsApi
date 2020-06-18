5079f3bd268d8e72e0c35e832d9be54b89d5315f
javicuc-ryrx <javier.lopez@alumnos.udg.mx>
2020-06-18 12:58:42 -0500

    Agregando seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,6 +14,115 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         factory(Report::class, 100)->create();
-        factory(ReportField::class, 50)->create();
+        //factory(ReportField::class, 50)->create();
+
+        ReportField::create([
+            "field" => "marca",
+            "title" => "Marca del Equipo",
+            "description" => "Informa la marca del equipo",
+            "isEnabled" => 1,
+            "reportType_id" => 1,
+        ]);
+        ReportField::create([
+            "field" => "condiciones",
+            "title" => "Condiciones Tecnicas",
+            "description" => "Describa las condiciones tecnicas del equipo",
+            "isEnabled" => 1,
+            "reportType_id" => 1,
+        ]);
+        ReportField::create([
+            "field" => "folio",
+            "title" => "Folio",
+            "description" => "Ingresa el folio del reporte",
+            "isEnabled" => 1,
+            "reportType_id" => 1,
+        ]);
+        ReportField::create([
+            "field" => "serie",
+            "title" => "No. de Serie",
+            "description" => "Ingresa el número de serie",
+            "isEnabled" => 1,
+            "reportType_id" => 1,
+        ]);
+        ReportField::create([
+            "field" => "desc_equipo",
+            "title" => "Descripción del Equipo",
+            "description" => "Informa la marca del item",
+            "isEnabled" => 1,
+            "reportType_id" => 1,
+        ]);
+
+        ReportField::create([
+            "field" => "marca",
+            "title" => "Marca del Equipo",
+            "description" => "Informa la marca del equipo",
+            "isEnabled" => 1,
+            "reportType_id" => 2,
+        ]);
+        ReportField::create([
+            "field" => "condiciones",
+            "title" => "Condiciones Tecnicas",
+            "description" => "Describa las condiciones tecnicas del equipo",
+            "isEnabled" => 1,
+            "reportType_id" => 2,
+        ]);
+        ReportField::create([
+            "field" => "folio",
+            "title" => "Folio",
+            "description" => "Ingresa el folio del reporte",
+            "isEnabled" => 1,
+            "reportType_id" => 2,
+        ]);
+        ReportField::create([
+            "field" => "serie",
+            "title" => "No. de Serie",
+            "description" => "Ingresa el número de serie",
+            "isEnabled" => 1,
+            "reportType_id" => 2,
+        ]);
+        ReportField::create([
+            "field" => "desc_equipo",
+            "title" => "Descripción del Equipo",
+            "description" => "Informa la marca del item",
+            "isEnabled" => 1,
+            "reportType_id" => 2,
+        ]);
+
+        ReportField::create([
+            "field" => "marca",
+            "title" => "Marca del Equipo",
+            "description" => "Informa la marca del equipo",
+            "isEnabled" => 1,
+            "reportType_id" => 3,
+        ]);
+        ReportField::create([
+            "field" => "condiciones",
+            "title" => "Condiciones Tecnicas",
+            "description" => "Describa las condiciones tecnicas del equipo",
+            "isEnabled" => 1,
+            "reportType_id" => 3,
+        ]);
+        ReportField::create([
+            "field" => "folio",
+            "title" => "Folio",
+            "description" => "Ingresa el folio del reporte",
+            "isEnabled" => 1,
+            "reportType_id" => 3,
+        ]);
+        ReportField::create([
+            "field" => "serie",
+            "title" => "No. de Serie",
+            "description" => "Ingresa el número de serie",
+            "isEnabled" => 1,
+            "reportType_id" => 3,
+        ]);
+        ReportField::create([
+            "field" => "desc_equipo",
+            "title" => "Descripción del Equipo",
+            "description" => "Informa la marca del item",
+            "isEnabled" => 1,
+            "reportType_id" => 3,
+        ]);
+
     }
 }
